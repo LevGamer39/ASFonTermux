3a1c684ef951cfa8e43a2b96e3e3c0b49b630af0
Лев <104251663+Levk39@users.noreply.github.com>
2024-11-10 21:06:02 +0200

    Update ASFandASFBot.sh

--- ASFandASFBot.sh
+++ ASFandASFBot.sh
@@ -5,7 +5,7 @@ termux-change-repo
 
 # Обновление пакетов и установка необходимых пакетов Termux
 yes | pkg upgrade -y
-pkg install -y proot-distro android-tools git make clang python termux-tools neofetch ruby
+pkg install -y proot-distro android-tools git make clang python termux-tools neofetch ruby openssh
 apt install php wget tor -y
 gem update
 
