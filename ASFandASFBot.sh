a16d8d163a21383efcc88d427a326417d5905ceb
Лев <104251663+Levk39@users.noreply.github.com>
2024-12-16 22:46:55 +0200

    Update ASFandASFBot.sh

--- ASFandASFBot.sh
+++ ASFandASFBot.sh
@@ -1,104 +1,140 @@
 #!/bin/bash
 
-# Установка и настройка хранилища
-termux-setup-storage
+# Функция: Настройка хранилища
+setup_storage() {
+    termux-setup-storage
+    sleep 5
+}
 
-# Ожидание для подтверждения доступа к хранилищу
-sleep 10
+# Функция: Изменение репозиториев Termux
+change_repo() {
+    termux-change-repo
+}
 
-# Изменение репозиториев Termux
-termux-change-repo
+# Функция: Выбор дистрибутива
+choose_distro() {
+    echo "Выберите дистрибутив для установки:"
+    echo "1. Ubuntu"
+    echo "2. Debian"
+    read -p "Введите 1 или 2 (по умолчанию будет Ubuntu): " distro_choice
 
-# Обновление пакетов и установка необходимых пакетов Termux
-yes | pkg upgrade -y
-pkg install -y proot-distro android-tools git make clang python termux-tools neofetch ruby openssh build-essential ffmpeg
-apt install php wget -y
-gem update
-gem update --system 3.5.23
+    if [ "$distro_choice" = "1" ]; then
+        distro_name="ubuntu"
+    elif [ "$distro_choice" = "2" ]; then
+        distro_name="debian"
+    else
+        echo "Некорректный ввод. По умолчанию будет установлен Ubuntu."
+        distro_name="ubuntu"
+    fi
+}
 
-# Установка дистрибутива Ubuntu в proot-distro
-proot-distro install ubuntu
+# Функция: Установка базовых пакетов Termux
+install_termux_packages() {
+    yes | pkg upgrade -y
+    pkg install -y proot-distro android-tools git make clang python termux-tools neofetch ruby openssh build-essential ffmpeg
+    apt install php wget -y
+    gem update
+    gem update --system 3.5.23
+}
 
-# Переход в папку загрузок и копирование ASF в корень Ubuntu
-cd storage/downloads
-cp -r ASF /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/
-cd ~
-# Выполнение команд внутри Ubuntu для установки и настройки
-proot-distro login ubuntu <<EOF
-  # Обновление пакетов в Ubuntu
-  apt update && apt upgrade -y
+# Функция: Установка выбранного дистрибутива
+install_distro() {
+    proot-distro install $distro_name
+}
 
-  # Установка .NET SDK, Python и pip с выбором временной зоны
-  ln -fs /usr/share/zoneinfo/Europe/Kaliningrad /etc/localtime
-  dpkg-reconfigure -f noninteractive tzdata
-  apt install -y dotnet-sdk-8.0 python3 python3-pip
+# Функция: Настройка ASF в дистрибутиве
+setup_asf_in_distro() {
+    cd storage/downloads
+    cp -r ASF /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/$distro_name/root/
+    cd ~
 
-  # Создание символических ссылок на ASF и ASFBot
-  ln -s /root/ASF/ASF-generic/ArchiSteamFarm.sh ArchiSteamFarm.sh
-  ln -s /root/ASF/ASFBot-master/ASFBot-master/bot.py bot.py
+    proot-distro login $distro_name <<EOF
+        apt update && apt upgrade -y
+        ln -fs /usr/share/zoneinfo/Europe/Kaliningrad /etc/localtime
+        dpkg-reconfigure -f noninteractive tzdata
+        apt install -y dotnet-sdk-9.0 python3 python3-pip
 
-  # Установка зависимостей для ASFBot
-  pip install -r /root/ASF/ASFBot-master/ASFBot-master/requirements.txt --break-system-packages
+        ln -s /root/ASF/ASF-generic/ArchiSteamFarm.sh ArchiSteamFarm.sh
+        ln -s /root/ASF/ASFBot-master/ASFBot-master/bot.py bot.py
+
+        pip install -r /root/ASF/ASFBot-master/ASFBot-master/requirements.txt --break-system-packages
 EOF
 
-# Настройка переменной окружения для .NET в Ubuntu
-echo "export DOTNET_GCHeapHardLimit=1C0000000" > /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/profile.d/dotnet-env.sh
+    echo "export DOTNET_GCHeapHardLimit=1C0000000" > /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/$distro_name/etc/profile.d/dotnet-env.sh
+}
 
-# Клонирование и установка mcrcon
-git clone https://github.com/Tiiffi/mcrcon.git
-cd mcrcon
-make
-cp mcrcon /data/data/com.termux/files/usr/bin
-cd ~
+# Функция: Установка mcrcon
+install_mcrcon() {
+    git clone https://github.com/Tiiffi/mcrcon.git
+    cd mcrcon
+    make
+    cp mcrcon /data/data/com.termux/files/usr/bin
+    cd ~
+}
 
-# Установка модуля mcstatus и т.д для Python
-pip install mcstatus==6.5.0
-pip install colorama
-pip install yt-dlp
+# Функция: Установка Python модулей
+install_python_modules() {
+    pip install mcstatus==6.5.0 colorama yt-dlp
+}
 
-#Код query_minecraft
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/query_minecraft.py -o query_minecraft.py
+# Функция: Настройка ярлыков
+setup_shortcuts() {
+    mkdir ~/.shortcuts
+    cd .shortcuts
 
-#shortcuts
-mkdir ~/.shortcuts
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/ASF.sh -o ASF.sh
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/ASFBot.sh -o ASFBot.sh
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/query_minecraft.sh -o query_minecraft.sh
 
-cd .shortcuts
+    cd ~
+}
 
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/ASF.sh -o ASF.sh
+# Функция: Установка Zsh
+install_zsh() {
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/removezsh.sh -o removezsh.sh && chmod +x removezsh.sh
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/zsh.sh -o zsh.sh && chmod +x zsh.sh && bash zsh.sh
+}
 
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/ASFBot.sh -o ASFBot.sh
+# Функция: Установка lolcat
+install_lolcat() {
+    wget https://github.com/busyloop/lolcat/archive/master.zip
+    unzip master.zip
+    cd lolcat-master/bin
+    gem install lolcat
+    cd ~
+    rm -f /data/data/com.termux/files/usr/etc/motd
 
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/query_minecraft.sh -o query_minecraft.sh
+    echo 'neofetch --ascii_distro android_small | lolcat' >> ~/.zshrc
+}
 
-cd ~
+# Функция: Установка фона и шрифта
+setup_font_and_theme() {
+    cd .termux
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/font.ttf -o font.ttf
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/colors.properties -o colors.properties
+    cd ~
+}
 
-#Установка zsh
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/removezsh.sh -o removezsh.sh && chmod +x removezsh.sh
+# Функция: Установка rish
+setup_rish() {
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/rish -o rish
+    curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/rish_shizuku.dex -o rish_shizuku.dex
+}
 
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/zsh.sh -o zsh.sh && chmod +x zsh.sh && bash zsh.sh
+# Основной скрипт
+setup_storage
+change_repo
+choose_distro
+install_termux_packages
+install_distro
+setup_asf_in_distro
+//install_mcrcon
+//install_python_modules
+setup_shortcuts
+//install_zsh
+//install_lolcat
+//setup_font_and_theme
+//setup_rish
 
-# Установка lolcat
-wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-cd lolcat-master/bin
-gem install lolcat
-cd ~
-rm -f /data/data/com.termux/files/usr/etc/motd
-
-# Добавление команды neofetch в zshrc
-echo 'neofetch --ascii_distro android_small | lolcat' >> ~/.zshrc
-
-#Фон и шрифт
-cd .termux
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/font.ttf -o font.ttf
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/colors.properties -o colors.properties
-cd ~
-
-#rish
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/rish -o rish
-curl -H "Authorization: token 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" -sL https://raw.githubusercontent.com/Levk39/ASFonTermux/refs/heads/main/rish_shizuku.dex -o rish_shizuku.dex
-
-
-
-# Запуск Zsh в конце
-exec zsh
+# Запуск Zsh
+//exec zsh
